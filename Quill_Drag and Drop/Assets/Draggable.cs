f65df913dbbe3c559733ed77b1d067f1016baffc
tech222 <tech222@hotmail.com>
2015-12-10 19:37:13 -0500

    Quill18 Drag and Drop Tutorial

---
+++ Quill_Drag and Drop/Assets/Draggable.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+	Vector2 dragOffset;
+	public Transform parentToReturnTo = null;
+	public Transform placeholderParent = null;
+    
+    GameObject placeholder = null;
+	
+	//public enum Slot {WEAPON, HEAD, CHEST, LEGS, FEET, INVENTORY};
+	//public Slot typeofItem = Slot.WEAPON;
+	
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		Debug.Log ("OnBeginDrag");
+        
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+		le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+		le.flexibleWidth = 0;
+		le.flexibleHeight = 0;
+		
+		placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+        
+		
+		dragOffset.x = this.transform.position.x - eventData.position.x;
+		dragOffset.y = this.transform.position.y - eventData.position.y;
+		
+        parentToReturnTo = this.transform.parent;
+        placeholderParent = parentToReturnTo;
+        transform.SetParent(transform.parent.parent);
+        
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        
+        /*
+        DropZone[] zone = GameObject.FindObjectOfType<DropZone>();
+        foreach(DropZone myZone in zone)
+        {
+        
+        }
+        */
+        
+	} 
+	
+	public void OnDrag(PointerEventData eventData)
+	{
+		//Debug.Log ("OnDrag");
+		
+		this.transform.position = eventData.position + dragOffset;
+		
+		int newSiblingIndex = placeholderParent.childCount;
+		
+		if(placeholder.transform.parent != placeholderParent)
+		{
+			placeholder.transform.SetParent(placeholderParent);
+		}
+		
+		for (int i=0; i < placeholderParent.childCount; i++)
+		{
+			if (this.transform.position.x < placeholderParent.GetChild(i).position.x)
+			{
+				newSiblingIndex = i;
+				
+				if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+				{
+					newSiblingIndex--;
+				}
+				break;
+			}
+		}
+		
+		placeholder.transform.SetSiblingIndex(newSiblingIndex);
+	}
+	
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		Debug.Log ("OnEndDrag");
+		transform.SetParent(parentToReturnTo);
+		this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+		
+		Destroy(placeholder);
+		
+		//EventSystem.current.RaycastAll(eventData);   // raycasts all objects under card
+	}
+}
---
+++ Quill_Drag and Drop/Assets/DropZone.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler 
+{
+	//public Draggable.Slot typeofItem = Draggable.Slot.INVENTORY;
+	
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		//Debug.Log ("OnDropEnter to " + gameObject.name);
+		if (eventData.pointerDrag == null)
+		{
+			return;
+		}
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null)
+		{
+			d.placeholderParent = this.transform;
+		}
+	}
+		
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		//Debug.Log ("OnDropExit to " + gameObject.name);
+		if (eventData.pointerDrag == null)
+		{
+			return;
+		}
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null && d.placeholderParent == this.transform)
+		{
+			d.placeholderParent = d.parentToReturnTo;
+		}
+	}
+	
+	public void OnDrop(PointerEventData eventData)
+	{
+		Debug.Log (eventData.pointerDrag.name + "was dropped on " + gameObject.name);
+		
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null)
+		{
+			d.parentToReturnTo = this.transform;
+		}
+			/*
+			if(typeofItem == d.typeofItem || typeofItem==Draggable.Slot.INVENTORY)
+			{
+				d.parentToReturnTo = this.transform;
+			}
+			*/
+	}
+
+}
